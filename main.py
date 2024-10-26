a7f0b8618b41f39f78a09a1dc70a271650ab48aa
CJ Ogbogu <ogbogu.pmc@gmail.com>
2024-10-26 05:26:03 +0100

    Update main.py
    The second step is to now declare the variables used to help establish a 'port' connection from the raspberry pi and the computer. all the requirements for hardware now have a declared variable.

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-# Starting with our imports. machine for hardwares pin connection, and utime for timing delays.
 from machine import Pin, PWM, Timer,I2C
 import utime
+
+# Buzzer
+buzzer = PWM(Pin(15))
+buzzer.duty_u16(1000)
+
+# Button
+button1 = Pin(10, Pin.IN, Pin.PULL_UP)
+button2 = Pin(11, Pin.IN, Pin.PULL_UP)
+button3 = Pin(12, Pin.IN, Pin.PULL_UP)
+
+# Police LED
+led1 = Pin(0, Pin.OUT) # Red
+led2 = Pin(1, Pin.OUT) # Blue
+
+# Ambulance LED
+led3 = Pin(2, Pin.OUT) # Green
+led4 = Pin(3, Pin.OUT) # Blue
+
+# Fire Brigade LED
+led5 = Pin(4, Pin.OUT) # Red
+led6 = Pin(5, Pin.OUT) # Red
+
+# Cancel Button
+button4 = Pin(14, Pin.IN, Pin.PULL_UP)
